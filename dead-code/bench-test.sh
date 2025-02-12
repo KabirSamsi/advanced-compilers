e7bfbe9f8d45e143ff7177887dde7ec94df99410
Kabir Samsi <samsikabir@gmail.com>
2025-02-12 11:45:23 -0500

    Propensity

--- dead-code/bench-test.sh
+++ dead-code/bench-test.sh
@@ -2,6 +2,6 @@ printf "\nInitial Implementation\n"
 cat benchmarks/core/$1.bril
 printf "\nFinal Implementation\n"
 bril2json < benchmarks/core/$1.bril > benchmarks/core/$1.json 
-bril2json < benchmarks/core/$1.bril | node dce.js  > benchmarks/core/$1-updated.json 
-# bril2json < benchmarks/core/$1.bril | node dce.js  | bril2txt
+# bril2json < benchmarks/core/$1.bril | node dce.js  > benchmarks/core/$1-updated.json 
+bril2json < benchmarks/core/$1.bril | node dce.js | bril2txt
 printf "\n"
