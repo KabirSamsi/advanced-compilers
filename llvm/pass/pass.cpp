28af00700bc9663b888747c3f4fde035a5d85e16
Kabir Samsi <samsikabir@gmail.com>
2025-03-20 03:42:45 -0400

    Config

--- llvm/pass/pass.cpp
+++ llvm/pass/pass.cpp
@@ -5,3 +5,31 @@
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
+
+namespace {
+
+struct L7Pass : public PassInfoMixin<L7Pass> {
+    PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
+        for (auto &F : M) {
+            errs() << "I saw a function called " << F.getName() << "!\n";
+        }
+        return PreservedAnalyses::all();
+    };
+};
+
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+    return {
+        .APIVersion = LLVM_PLUGIN_API_VERSION,
+        .PluginName = "L7 Pass (WIP)",
+        .PluginVersion = "v0.1",
+        .RegisterPassBuilderCallbacks = [](PassBuilder &PB) {
+            PB.registerPipelineStartEPCallback(
+                [](ModulePassManager &MPM, OptimizationLevel Level) {
+                    MPM.addPass(L7Pass());
+                });
+        }
+    };
+}
